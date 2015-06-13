7c04d5f7c143f229ba7fd46a3c77e74915848ce6
liuhuixiao <liuhuixiao@gintong.com>
2015-06-13 20:50:58 +0800

    first

---
+++ config/default.js
@@ -0,0 +1,254 @@
+var plugins = {
+  define : require('../lib/postprocessor/define.js'),
+  annotate : require('../lib/postprocessor/annotate.js'),
+  vender : require('../lib/prepackager/vender.js'),
+  //uaeConf : require('../lib/prepackager/uae-conf.js'),
+  frameworkConf : require('../lib/postpackager/framework-conf.js')
+};
+
+//console.log(fis.config.get('name'), fis.config.get('version'))
+
+module.exports = {
+  project: {
+    fileType: {
+      text : 'handlebars, jade, ejs, jsx, styl, tag'
+    },
+    exclude: /(node_modules\/.*)|(bower_modules\/.*)|(dist\/.*)/
+  },
+  modules: {
+    parser: {
+    },
+    lint: {
+      js: 'jshint'
+    },
+    postprocessor: {
+      js: [
+        plugins.define,
+        plugins.annotate
+      ]
+    },
+    prepackager: [
+      plugins.vender
+    ],
+    postpackager: [
+      plugins.frameworkConf
+    ],
+    optimizer: {
+      png: 'pngcrush',
+      json: function(content){
+        return JSON.stringify(JSON.parse(content));
+      }
+    },
+    deploy: ['default', 'compress']
+  },
+  component: {
+    dir: '/component_modules'
+  },
+  projectPrefix: '',
+  framework: {
+    cache: true,
+    urlPattern: '/%s',
+    comboPattern: '/co??%s'
+  },
+  roadmap: {
+    ext: {
+    },
+    path: [
+      //{
+      //  reg: /.*\.tpl\.html$/,
+      //  release: false
+      //},
+      {
+        reg : '**.md',
+        isHtmlLike : true,
+        release : false
+      },
+      {
+        reg : /\.inline\.\w+$/i,
+        release : false
+      },
+      {
+        reg: /^\/test\/test-main\.js$/,
+        isViews: true,
+        isTest: true,
+        useHash : false,
+        release: '/test/test-main.js'
+      },
+      {
+        reg: /^\/test\/(.*\.spec\.js)$/,
+        id : '$1',
+        isJsLike: true,
+        isMod : true,
+        isTest: true,
+        useHash : false,
+        release: '/test/${projectPrefix}/$1'
+      },
+      {
+        reg: /^\/components\/(.*\.spec\.js)$/,
+        id : '$1',
+        isJsLike: true,
+        isMod : true,
+        isTest: true,
+        useHash : false,
+        release: '/test/${projectPrefix}/$1'
+      },
+      {
+        reg : /^\/components\/(.*\.js)$/i,
+        id : '$1',
+        isMod : true,
+        useHash : false,
+        url : '${projectPrefix}/lib/$1',
+        release : '/public/${projectPrefix}/lib/$1'
+      },
+      {
+        reg : /^\/components\/(.*)\.(styl|css)$/i,
+        id : '$1.css',
+        isMod : true,
+        useSprite : true,
+        useHash : false,
+        url : '${projectPrefix}/lib/$1.$2',
+        release : '/public/${projectPrefix}/lib/$1.$2'
+      },
+      {
+        reg : /^\/components\/(.*)\.tpl\.html$/i,
+        id : '$1',
+        isMod : true,
+        isTemplate : true,
+        isHtmlLike: true,
+        useHash : false,
+        url : '${projectPrefix}/lib/$1.tpl.html',
+        release : '/public/${projectPrefix}/lib/$1.tpl.html'
+      },
+      {
+        reg : /^\/components\/(.*)\.json$/i,
+        url : '${projectPrefix}/lib/$1',
+        useHash : false,
+        release : '/public/${projectPrefix}/lib/$1'
+      },
+      {
+        reg : /^\/components\/(.*)$/i,
+        url : '${projectPrefix}/lib/$1',
+        release : '/public/${projectPrefix}/lib/$1'
+      },
+      {
+        reg : /^\/component_modules\/(.*\.js)$/i,
+        id : 'c/$1',
+        isMod : true,
+        useHash : false,
+        url : '${projectPrefix}/lib/c/$1',
+        release : '/public/${projectPrefix}/lib/c/$1'
+      },
+      {
+        reg : /^\/component_modules\/(.*)\.(styl|css)$/i,
+        id : 'c/$1.css',
+        isMod : true,
+        useSprite : true,
+        useHash : false,
+        url : '${projectPrefix}/lib/c/$1.$2',
+        release : '/public/${projectPrefix}/lib/c/$1.$2'
+      },
+      {
+        reg : /^\/component_modules\/(.*)\.tpl\.html$/i,
+        id : 'c/$1',
+        isMod : true,
+        isTemplate : true,
+        isHtmlLike: true,
+        useHash : false,
+        url : '${projectPrefix}/lib/c/$1.tpl.html',
+        release : '/public/${projectPrefix}/lib/c/$1.tpl.html'
+      },
+      {
+        reg : /^\/component_modules\/(.*)$/i,
+        url : '${projectPrefix}/lib/c/$1',
+        useHash : false,
+        release : '/public/${projectPrefix}/lib/c/$1'
+      },
+      {
+        reg : /^\/views\/(favicon\.(?:png|ico))$/,
+        useHash : false,
+        url : '${projectPrefix}/$1',
+        release : '/public/$1'
+      },
+      {
+        reg : /^\/views\/(.*\.(?:html?|js))$/,
+        useCache : false,
+        //useHash : false,
+        isViews : true,
+        isHtmlLike: true,
+        url : '${projectPrefix}/$1',
+        release : '/public/${projectPrefix}/$1'
+      },
+      {
+        reg : /^\/views\/(.*)$/,
+        useSprite : true,
+        //useHash : false,
+        isViews : true,
+        url : '${projectPrefix}/$1',
+        release : '/public/${projectPrefix}/$1'
+      },
+      {
+        reg : 'map.json',
+        release : false
+      },
+      {
+        reg : '**',
+        useHash : false,
+        useCompile : false
+      }
+    ]
+  },
+  settings: {
+    vender: {
+
+    },
+    optimizer : {
+      "uglify-js" : {
+        mangle: {
+          except: ["require", "exports", "module", "window"]
+        },
+        compress: {
+          "global_defs": {
+            PROD: true
+          },
+          "dead_code": true,
+          "pure_funcs": [
+            "console.log",
+            "console.info",
+            "console.warn",
+            "console.error",
+            "console.assert",
+            "console.count",
+            "console.clear",
+            "console.group",
+            "console.groupEnd",
+            "console.groupCollapsed",
+            "console.trace",
+            "console.debug",
+            "console.dir",
+            "console.dirxml",
+            "console.profile",
+            "console.profileEnd",
+            "console.time",
+            "console.timeEnd",
+            "console.timeStamp",
+            "console.table",
+            "console.exception"
+          ]
+        }
+      }
+    },
+    deploy: {
+      default: {
+        //gtfe release -d local
+        local: {
+          to: '../dist'
+        }
+      },
+      //gtfe release -d zip
+      compress: {
+        zip: {
+        }
+      }
+    }
+  }
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+var fis = exports.fis = require('fis');
+fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+fis.cli.name = fis.cli.info.name;
+
+//plugin search order
+fis.require.prefixes = [ fis.cli.name, 'scrat', 'fis' ];
+
+//merge config
+var config = require('./config/default.js');
+fis.config.merge(config);
+
+//register command plugins
+['publish', 'init'].forEach(function(name){
+  fis.require._cache['command-' + name] = require('./lib/command/' + name);
+});
+
+//register parser plugins
+[].forEach(function(name){
+  fis.require._cache['parser-' + name] = require('./lib/parser/' + name);
+});
+
+//register deploy plugins
+['compress'].forEach(function(name){
+  fis.require._cache['deploy-' + name] = require('./lib/deploy/' + name);
+});
+
+//alias
+Object.defineProperty(global, fis.cli.name, {
+  enumerable : true,
+  writable : false,
+  value : fis
+});
+
+//version
+fis.cli.version = function(){
+  var content = [
+    '  v' + fis.cli.info.version
+  ].join('\n');
+  console.log(content);
+};
+
+//check update
+var updateNotifier = require('update-notifier');
+updateNotifier({pkg: fis.cli.info}).notify();
+
+//warning
+if(fis.util.isFile('fis-conf.js') && fis.util.isDir('node_modules')){
+  fis.log.warning('source folder `node_modules` will cause fis compile slow, plz delete it.')
+}
